f755fe3fdc7bf6e782fbd6d4acf9173386af791b
Jamie Wilkinson <jamie@tramchase.com>
2009-07-16 21:01:53 -0400

    Added jQuery with UI and form plugin

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,34 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {});
+
+var PNB = function() {
+  return {
+    formFocusFirst: function(form) {
+      $('#' + form + ' input:visible:enabled:first').focus()
+    },
+
+    updateSortables: function(parent) {
+      var elems = $(parent + ' ul li .position'), i = 1
+      elems.each(function() {
+        this.value = i++
+      })
+    },
+
+    sizeTehToolbars: function() {
+      $('.textile-toolbar').each(function() {
+        box_id = this.id.replace('textile-toolbar-', '')
+        $(this).css('width', $('#' + box_id).css('width'))
+      })
+    },
+
+    load_gat: function(code) {
+      if (code) {
+        var host = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+        jQuery.getScript(host + "google-analytics.com/ga.js", function(){
+          var tracker = _gat._getTracker(code);
+          tracker._initData();
+          tracker._trackPageview();
+        });
+      }
+    }
+  };
+}();
