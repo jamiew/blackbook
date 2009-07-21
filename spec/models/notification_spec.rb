74d26e8e9ff43614679cf2fca323e79d2c91ff21
Jamie Wilkinson <jamie@tramchase.com>
2009-07-21 18:58:16 -0400

    typo

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -18,12 +18,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Notification do
   before(:each) do
     @valid_attributes = {
-      :subject_id => "value for subject_id",
-      :subject_type => "value for subject_type",
-      :verb => "value for verb",
+      :subject_id => 1,
+      :subject_type => "Comment",
+      :verb => "created",
       :user_id => 1,
       :supplement_id => 1,
-      :supplement_type => "value for supplement_type"
+      :supplement_type => "Video"
     }
   end
 
