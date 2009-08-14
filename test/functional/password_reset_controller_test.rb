87ac2d457a218729dd4c945db9eddb7f3a0a39a9
Jamie Wilkinson <jamie@tramchase.com>
2009-08-13 23:52:04 -0700

    converting to rspec

--- test/functional/password_reset_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class PasswordResetControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
--- test/unit/helpers/visualizations_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class VisualizationsHelperTest < ActionView::TestCase
-end
