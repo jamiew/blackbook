1c6ebdf04b4628b40a9f0af3fe892984d6ac5c1f
Jamie Wilkinson <jamie@tramchase.com>
2009-08-21 10:11:05 -0700

    limit frontpage to 7 latest

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    @tags = Tag.find(:all, :order => 'created_at DESC', :limit => 12)
+    @tags = Tag.find(:all, :order => 'created_at DESC', :limit => 7)
     @users = User.find(:all, :order => 'created_at DESC', :limit => 10)
   end
   
