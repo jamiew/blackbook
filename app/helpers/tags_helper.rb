d808acb0139f8d7e33e04ee89b7ff1f9b0ab6a49
Jamie Wilkinson <jamie@tramchase.com>
2009-12-14 19:03:12 -0800

    prototype support for Todd Vanderlin's playback app, and some ghetto tabs

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -3,17 +3,34 @@ module TagsHelper
   # Tag flash visualizer -- allow people to customize
   # If no tag specified try to do "slideshow" mode (??)
   def tag_player(tag = nil, args = {})
-    opts = { :width => '100%', :height => '100%', :src => 'BlackBook.swf', :bgcolor => '#000000' }.merge(args)
+    STDERR.puts tag.inspect
+    opts = { :width => '100%', :height => '100%', :src => 'http://toddvanderlin.com/content/000000book/BlackBook.swf', :bgcolor => '#000000' }.merge(args)
     %{    
       <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="#{opts[:width]}" height="#{opts[:height]}" id="BlackBook" align="middle">
+        <!-- typical shit -->
         <param name="allowScriptAccess" value="sameDomain" />
-        <param name="allowFullScreen" value="false" />
-        <param name="movie" value="/#{opts[:src]}" />
+        <param name="allowFullScreen" value="true" />
         <param name="quality" value="high" />
         <param name="scale" value="noscale" />
         <param name="wmode" value="gpu" />
-        <param name="bgcolor" value="#{opts[:bgcolor]}" />	
-        <embed src="/#{opts[:src]}" quality="high" scale="noscale" wmode="gpu" bgcolor="#{opts[:bgcolor]}" width="#{opts[:width]}" height="#{opts[:height]}" name="BlackBook" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" />
+        <param name="loop" value="true" />
+        <param name="movie" value="#{opts[:src]}" />
+        <param name="bgcolor" value="#{opts[:bgcolor]}" />
+        
+        <!-- blackbook data -->
+        <param name="blackbook_gml_url" value="#{tag_url(tag, :format => 'gml')}" />
+        <param name="blackbook_embed" value="&lt;embed&gt;TODOWHATUP&lt;/embed&gt;" />
+        <param name="blackbook_id" value="#{tag.id}" />
+        <param name="blackbook_application" value="#{tag.application}" />
+        <param name="blackbook_user" value="#{tag.user.login rescue ''}" />
+        <param name="blackbook_user_id" value="#{tag.user_id}" />
+        <param name="blackbook_location" value="#{tag.location}" />
+        <param name="blackbook_image_large" value="#{tag.image.url(:large)}" />
+        <param name="blackbook_image_medium" value="#{tag.image.url(:medium)}" />
+        <param name="blackbook_image_small" value="#{tag.image.url(:small)}" />
+        
+        <!-- embed itself -->
+        <embed src="#{opts[:src]}" quality="high" scale="noscale" wmode="gpu" bgcolor="#{opts[:bgcolor]}" width="#{opts[:width]}" height="#{opts[:height]}" name="BlackBook" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" />
       </object>
     }
   end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,53 +1,30 @@
-$(function() {});
-
-var PNB = function() {
-  return {
-    formFocusFirst: function(form) {
-      $('#' + form + ' input:visible:enabled:first').focus();
-    },
-
-    updateSortables: function(parent) {
-      var elems = $(parent + ' ul li .position'), i = 1;
-      elems.each(function() {
-        this.value = i++;
-      });
-    },
-
-    sizeTehToolbars: function() {
-      $('.textile-toolbar').each(function() {
-        box_id = this.id.replace('textile-toolbar-', '');
-        $(this).css('width', $('#' + box_id).css('width'));
-      });
-    },
-
-    load_gat: function(code) {
-      if (code) {
-        var host = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-        jQuery.getScript(host + "google-analytics.com/ga.js", function(){
-          var tracker = _gat._getTracker(code);
-          tracker._initData();
-          tracker._trackPageview();
-        });
-      }
-    }
-  };
-}();
-
+//
+// Blackbook javascript
+// 
 
 $(document).ready(function(){
 
-  // flashing
+  // Flashes
   $('#flash-error, #flash-notice, #flash-warning').slideToggle('slow');
   setTimeout(function(){
     $('#flash-error, #flash-notice, #flash-warning').slideToggle('slow');
   }, 2500);
   
-  // table stylez
-  // $("div:odd").css("background-color", "#F4F4F8");
-  // $("div:even").css("background-color", "#EFF1F1");
-
-  //for table row
+  // Zebrafy table rows
   $("tr:even").css("background-color", "#000");
   $("tr:odd").css("background-color", "#111");
+  
+  // Ghetto tabs
+	var tabContainers = $('div.tabs > div');
+	tabContainers.hide().filter(':first').show();
+	
+	$('div.tabs ul.tab_navigation a').click(function () {
+		tabContainers.hide();
+		tabContainers.filter(this.hash).show();
+		$('div.tabs ul.tab_navigation a').removeClass('selected');
+		$(this).addClass('selected');
+		return false;
+	}).filter(':first').click();
+  
   
 });
