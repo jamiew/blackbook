5e14d237112577aa59e0f7d9118d0f424ae99529
Jamie Wilkinson <jamie@tramchase.com>
2009-12-17 10:08:03 -0800

    don't specify any kind of height for the flash player

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -4,7 +4,8 @@ module TagsHelper
   # If no tag specified try to do "slideshow" mode (??)
   def tag_player(tag = nil, args = {})
     
-    opts = { :width => '100%', :height => '100%', :src => 'http://toddvanderlin.com/content/000000book/BlackBook.swf', :bgcolor => '#000000' }.merge(args)
+    # No longer specifying a specific height, just width
+    opts = { :width => '100%', :src => 'http://toddvanderlin.com/content/000000book/BlackBook.swf', :bgcolor => '#000000' }.merge(args)
     
     # image_urls = tag.image.styles.keys.map { |s| ["image_#{s}", "http://#{request.host}:#{request.port}"+tag.image.url(s)] }.to_hash
     image_urls = {:image_large => tag.image.url(:large)}
@@ -17,10 +18,10 @@ module TagsHelper
     flashvars.merge!( tag.attributes.select { |k,v| public_attributes.include?(k) } )
 
     querified_flashvars = flashvars.map { |k,v| "#{k}=#{v.blank? ? '' : CGI.escape(v.to_s)}" }.join('&')
-    embed = %{<embed src="#{opts[:src]}?#{querified_flashvars}" quality="high" scale="noscale" wmode="gpu" loop="true" bgcolor="#{opts[:bgcolor]}" width="#{opts[:width]}" height="#{opts[:height]}" name="BlackBook" align="middle" allowScriptAccess="always" allowFullScreen="true" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" />}
+    embed = %{<embed src="#{opts[:src]}?#{querified_flashvars}" quality="high" scale="noscale" wmode="gpu" loop="true" bgcolor="#{opts[:bgcolor]}" width="#{opts[:width]}" name="BlackBook" align="middle" allowScriptAccess="always" allowFullScreen="true" type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" />}
     
     %{    
-      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="#{opts[:width]}" height="#{opts[:height]}" id="BlackBook" align="middle">
+      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="#{opts[:width]}" id="BlackBook" align="middle">
 
         <!-- typical flash noise; note allowScriptAccess (for js), allowFullScreen=true, loop=true, bgcolor=BLACK -->
         <param name="allowScriptAccess" value="always" />
