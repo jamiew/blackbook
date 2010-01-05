238096b08553a1e299f8f981e31ee32e350c77be
Jamie Wilkinson <jamie@tramchase.com>
2010-01-04 18:50:18 -0800

    new default images

--- app/models/visualization.rb
+++ app/models/visualization.rb
@@ -45,6 +45,13 @@ class Visualization < ActiveRecord::Base
   named_scope :approved, { :conditions => ['approved_at > ?', Time.now] }
   named_scope :pending, { :conditions => ['approved_at IS NULL OR approved_at < ?', Time.now] }
   
+  # has_attached_file :image, 
+  #   :default_style => :medium,
+  #   :default_url => "/images/defaults/app_:style.jpg",
+  #   :styles => { :large => '600x600>', :medium => "300x300>", :small => '100x100#', :tiny => "32x32#" }
+  # validates_attachment_presence :image
+  
+  
   def approved?
     self.approved_at && self.approved_at < Time.now
   end
