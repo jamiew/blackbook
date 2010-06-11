11706202c5e9277a1677d758314c3eb01ef0fb4f
Jamie Wilkinson <jamie@tramchase.com>
2010-06-11 00:33:52 -0400

    flush memcache b/w test cases

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,9 @@ Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+
+  # Flush memcache b/w every test case
+  config.before(:each) do
+    $memcache.flush_all unless $memcache.nil?
+  end
 end
