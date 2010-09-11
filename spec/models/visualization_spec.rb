9d32e94ee2c03065d2584ac03d9424155217bfb1
Jamie Wilkinson <jamie@tramchase.com>
2010-09-11 15:45:36 -0700

    strip annotations from specs

--- spec/models/visualization_spec.rb
+++ spec/models/visualization_spec.rb
@@ -1,15 +1,18 @@
+require File.dirname(__FILE__) + '/../spec_helper'
 
-# == Schema Information
-#
-# Table name: visualizations
-#
-#  id          :integer(4)      not null, primary key
-#  user_id     :integer(4)
-#  name        :string(255)
-#  slug        :string(255)
-#  website     :string(255)
-#  download    :string(255)
-#  version     :string(255)
-#  description :text
-#
+describe Visualization do
 
+  it "should create" do
+    user = Factory.build(:user)
+    user.save!
+  end
+
+  it "should fail without a login" do
+    lambda { Factory.create(:user, :login => '') }.should raise_error
+  end
+
+  it "should fail without an email" do
+    lambda { Factory.create(:user, :email => '') }.should raise_error
+  end
+
+end
