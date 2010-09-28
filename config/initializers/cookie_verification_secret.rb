373489d8e67d0bce82678e10456021bbb5deb861
Jamie Wilkinson <jamie@tramchase.com>
2010-09-28 00:55:55 -0700

    Add/update Rails 2.3.9 support files

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '9a61a29994ae5073ecd3b66affaa31852634c367ab97cc287124783b48531c88abcb7809136040363e1f11c43d8fe6cfe1959004cf984e9a99b9c67f115b3773';
