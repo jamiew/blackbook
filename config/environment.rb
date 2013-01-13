3b4299639376d2cb8c6067e5f35d3c6432d6d91d
Jamie Wilkinson <jamie@jamiedubs.com>
2013-01-13 01:10:09 -0800

    Upgrade to rails 2.3.15

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@
 #                                                            #
  ############################################################
 
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.15' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
