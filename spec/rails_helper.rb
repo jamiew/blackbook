7116653636f0387538d79cda1cc4457184fa5b98
Jamie Wilkinson <jamie@jamiedubs.com>
2017-06-18 13:01:59 -0400

    Sanity check RAILS_ENV=test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,13 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['Rails.env'] ||= 'test'
+# Try to be sure we are running in RAILS_ENV=test
+ENV['Rails.env'] = 'test'
+ENV['RAILS_ENV'] = 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort("The Rails environment is running in development mode!") if Rails.env.development?
+
 require 'spec_helper'
 
 require 'rspec/rails'
@@ -12,9 +17,6 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
