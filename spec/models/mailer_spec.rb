9466b92e30e4b227336ab18e0fe275bfced5c321
Jamie Wilkinson <jamie@jamiedubs.com>
2017-06-18 13:41:50 -0400

    Pending tests for Mailer

---
+++ spec/models/mailer_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Mailer, type: :mailer do
+  describe 'password_reset_instructions' do
+    it 'works'
+  end
+
+  describe 'signup_notification' do
+    it 'works'
+  end
+end
