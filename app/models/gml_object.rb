6ff27757bdac189e3967c094900508b6ddec63e2
Jamie Wilkinson <jamie@jamiedubs.com>
2017-07-11 22:50:41 -0400

    Read and save GmlObject data to disk
    And later, to S3 and/or IPFS

--- app/models/gml_object.rb
+++ app/models/gml_object.rb
@@ -6,6 +6,10 @@ class GmlObject < ActiveRecord::Base
   validates_uniqueness_of :tag_id, :on => :create, :message => "must be unique"
   validates_associated :tag, :on => :create
 
+  after_save :store_on_disk
+  # after_save :store_on_s3
+  # after_save :store_on_ipfs
+
   # FIXME temporarily allowing blank data...
   # Need to make the process a little more clear; FIXME. Also some magic in TagsController to link into this
   # validates_presence_of :data, :on => :create, :message => "can't be blank"
@@ -27,5 +31,59 @@ class GmlObject < ActiveRecord::Base
   # TODO validate GML here instead of Tag
   # can also do header extraction and such
   # as well as to_json? not sure.
+
+  def self.read_all_cached_gml
+    Dir.glob(Rails.root + 'public/gml-real/*.gml').each do |path|
+      id = path.match(/.+\/(.+)\.gml/)[1]
+      tag = Tag.find_by_id(id)
+      if tag.nil?
+        $stderr.puts "Could not find Tag #{id} for path=#{path.inspect}, skipping"
+        next
+      end
+
+      if tag.gml_object.blank?
+        $stderr.puts "No GmlObject for Tag #{id}, creating"
+        tag.send(:build_gml_object) # sorry
+        tag.send(:save_gml_object) # really I mean it
+      end
+
+      tag.gml_object.update_data_from_file(path)
+    end
+  end
+
+  def update_data_from_file(path)
+    if self.data.present?
+      $stderr.puts "GMLObject data exists for tag, #{self.data.size} bytes, skipping"
+      return false
+    end
+
+    self.data = File.open(path).read
+    puts "GmlObject.data now #{self.data.size} bytes"
+    success = self.save
+    if !success
+      $stderr.puts "Error saving GMLObject: #{self.errors.to_json.inspect}"
+    end
+    success
+  end
+
+  def store_on_disk
+    filename = "#{Rails.root}/public/gml/#{tag_id}.gml"
+    File.open(filename, 'w+') do |file|
+      file.write(data)
+    end
+  end
+
+  def store_on_s3
+    # Do some Amazon::SDK and stick it on S3
+  end
+
+  def store_on_ipfs
+    # fuck yeah
+    # TODO test that daemon is running
+    # run as part of Procfile?
+    ipfs = IPFS::Connection.new
+
+
+  end
 
 end
