caea6888af7126175714cf3ceb1354982129cd99
Jamie Wilkinson <jamie@jamiedubs.com>
2018-03-18 21:36:38 -0400

    Cleanup GML downloader

--- public/download-gml.sh
+++ public/download-gml.sh
@@ -3,7 +3,8 @@
 dir="./data"
 mkdir $dir
 
-i=59
+# i=59
+i=54000
 while [ 1 ]; do
 
   i=$((i + 1))
@@ -17,6 +18,7 @@ while [ 1 ]; do
   fi
 
   wget "http://000000book.com/data/$i.gml" -O "$outfile"
+  # wget "http://000000book.com/data/$i.json" -O "$outfile"
 
   if [ ! -s "$outfile" ] ; then
     echo "$outfile is empty, deleting..."
