77bda86870bf4318d3989eab7851f31bc06e8ac9
Jamie Wilkinson <jamie@jamiedubs.com>
2019-03-01 11:07:51 -0500

    Fix broken homepage styles and behaviors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
 
   def index
     @tags = Tag.order('created_at DESC').limit(30).includes(:user)
-    @tag = @tags.present? && @tags.first # formerly .shift to pop it off, should do that on frontend
+    @tag = @tags.present? && @tags[0] # formerly .shift to pop it off, should do that on frontend
+    @prev = @tags.present? && @tags[1]
     set_page_title("#000000book - an open database for Graffiti Markup Language (GML) files", false)
   end
 
