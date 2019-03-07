a3e1638a5f1e803f6be570d8bcedd7805b804a83
Jamie Wilkinson <jamie@jamiedubs.com>
2019-03-07 08:10:25 -0500

    Force SSL on the homepage too, which should persist through session

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
   # caches_action :index, cache_path: 'home/index', expires_in: 30.minutes, if: :cache_request?
 
+  force_ssl
+
   def index
     @tags = Tag.order('created_at DESC').limit(30).includes(:user)
     @tag = @tags.present? && @tags[0] # formerly .shift to pop it off, should do that on frontend
