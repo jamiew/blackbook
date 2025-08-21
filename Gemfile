4d3c6695cd7147c0f03dbd2a2a26529bde676b53
Jamie Dubs <jamie@jamiedubs.com>
2025-08-21 14:32:48 -0400

    YOLO all the way to Rails 8

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 ruby File.open(File.dirname(__FILE__)+'/.ruby-version', 'rb') { |f| f.read.chomp }
 
-gem 'rails', '~> 7.2.0'
+gem 'rails', '~> 8.0.2'
 gem 'mysql2', '~> 0.5.6'
 gem 'puma', groups: [:development, :production]
 
--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,10 @@ module Blackbook4
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Rails 7 configuration
-    config.load_defaults 7.1
+    # Rails 8 configuration
+    config.load_defaults 8.0
+    
+    # Fix timezone deprecation warning
+    config.active_support.to_time_preserves_timezone = :zone
   end
 end
